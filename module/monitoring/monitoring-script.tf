bb17ad89b4e7e2cce7b29f8e3ca06e41a3c766c3
tundeafod <134690235+tundeafod@users.noreply.github.com>
2024-05-16 00:09:23 +0100

    Update monitoring-script.tf

--- module/monitoring/monitoring-script.tf
+++ module/monitoring/monitoring-script.tf
@@ -4,6 +4,30 @@ locals {
 
 sudo apt update
 
+# Update instance and install tools (wget, unzip, aws cli) 
+sudo apt install wget unzip -y
+wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#configuring the aws cli on your ubuntu user
+sudo su -c "aws configure set aws_access_key_id ${aws_iam_access_key.prom_user_access_key.id}" ubuntu
+sudo su -c "aws configure set aws_secret_access_key ${aws_iam_access_key.prom_user_access_key.secret}" ubuntu
+sudo su -c "aws configure set default.region eu-west-2" ubuntu
+sudo su -c "aws configure set default.output text" ubuntu
+
+#make access key for environment variable
+export AWS_ACCESS_KEY_ID=${aws_iam_access_key.prom_user_access_key.id}
+export AWS_SECRET_ACCESS_KEY=${aws_iam_access_key.prom_user_access_key.secret}
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_DEFAULT_OUTPUT=text
+
+# Disable SSH strict host checking
+sudo bash -c 'echo "StrictHostKeyChecking No" >> /etc/ssh/ssh_config'
+
+# Add AWS CLI to system PATH
+sudo ln -svf /usr/local/bin/aws /usr/bin/aws
+
 # create a group and user 
 sudo groupadd --system prometheus
 sudo useradd -s /sbin/nologin --system -g prometheus prometheus
@@ -11,9 +35,9 @@ sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 
 # download the prometheus tar file for the internet and configure it
-wget https://github.com/prometheus/prometheus/releases/download/v2.45.5/prometheus-2.45.5.linux-amd64.tar.gz
+wget https://github.com/prometheus/prometheus/releases/download/v2.43.0/prometheus-2.43.0.linux-amd64.tar.gz
 tar vxf prometheus*.tar.gz
-cd prometheus-2.45.5.linux-amd64
+cd prometheus-2.43.0.linux-amd64
 sudo mv prometheus /usr/local/bin
 sudo mv promtool /usr/local/bin
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
@@ -26,30 +50,16 @@ sudo chown prometheus:prometheus /etc/prometheus
 sudo chown -R prometheus:prometheus /etc/prometheus/consoles
 sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 sudo chown -R prometheus:prometheus /var/lib/prometheus
-sudo chmod 747 /etc/prometheus/prometheus.yml
 
-# create prometheus config file
-sudo cat <<EOT> /etc/prometheus/prometheus.yml
-global:
-  scrape_interval: 15s
-  external_labels:
-    monitor: "prometheus"
+cd
+rm -rf prometheus-2.43.0.linux-amd64.tar.gz prometheus-2.43.0.linux-amd64
 
-scrape_configs:
-  - job_name: "prometheus"
-
-  - job_name: "infra_node_exporter"
-    static_configs:
-      - targets: ["localhost:9100", "${var.nexus-ip}:9100", "${var.jenkins_ip}:9100", "${var.Sonarqube-ip}:9100", "${var.ansible_ip}:9100"]
-
-  - job_name: "ec2-service-discovery"
-    ec2_sd_configs:
-      - region: eu-west-2
-        port: 9100
-EOT
-
-# cd 
-# rm -rf prometheus-2.45.5.linux-amd64.tar.gz prometheus-2.45.5.linux-amd64
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
 
 # create prometheus service file to start prometheus
 sudo cat <<EOT>> /etc/systemd/system/prometheus.service
@@ -57,7 +67,6 @@ sudo cat <<EOT>> /etc/systemd/system/prometheus.service
 Description=Prometheus
 Wants=network-online.target
 After=network-online.target
-
 [Service]
 User=prometheus
 Group=prometheus
@@ -67,9 +76,27 @@ ExecStart=/usr/local/bin/prometheus \
     --storage.tsdb.path /var/lib/prometheus/ \
     --web.console.templates=/etc/prometheus/consoles \
     --web.console.libraries=/etc/prometheus/console_libraries
-
 [Install]
 WantedBy=multi-user.target
+EOT
+
+# create prometheus config file
+sudo cat <<EOT> /etc/prometheus/prometheus.yml
+global:
+  scrape_interval: 15s
+  external_labels:
+    monitor: 'prometheus'
+
+scrape_configs:
+  - job_name: 'Infra node exporter'
+    static_configs:
+      - targets: ['localhost:9100', '${var.nexus-ip}:9100', '${var.jenkins_ip}:9100', '${var.Sonarqube-ip}:9100', '${var.ansible_ip}:9100']
+
+  - job_name: 'ec2-service-discovery'
+    ec2_sd_configs:
+      - region: eu-west-2
+        access_key: '${aws_iam_access_key.prom_user_access_key.id}'
+        secret_key: '${aws_iam_access_key.prom_user_access_key.secret}'
 EOT
 
 sudo systemctl daemon-reload
@@ -80,12 +107,12 @@ sudo systemctl start prometheus
 sudo useradd --no-create-home node_exporter
 
 # download node_exporter tar file
-wget https://github.com/prometheus/node_exporter/releases/download/v1.8.0/node_exporter-1.8.0.linux-amd64.tar.gz
-tar xzf node_exporter-1.8.0.linux-amd64.tar.gz
-cd node_exporter-1.8.0.linux-amd64
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.6.1.linux-amd64.tar.gz
+cd node_exporter-1.6.1.linux-amd64
 sudo cp node_exporter /usr/local/bin
 cd ..
-rm -rf node_exporter-1.8.0.linux-amd64.tar.gz node_exporter-1.8.0.linux-amd64
+rm -rf node_exporter-1.6.1.linux-amd64.tar.gz node_exporter-1.6.1.linux-amd64
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 # create node_exporter service file to start node_exporter
@@ -120,18 +147,6 @@ sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 sudo apt update
 sudo apt -y install grafana
 sudo systemctl enable --now grafana-server
-
-# Update instance and install tools (wget, unzip, aws cli) 
-sudo apt install wget unzip -y
-wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-# Disable SSH strict host checking
-sudo bash -c 'echo "StrictHostKeyChecking No" >> /etc/ssh/ssh_config'
-
-# Add AWS CLI to system PATH
-sudo ln -svf /usr/local/bin/aws /usr/bin/aws
 
 sudo hostnamectl set-hostname monitoring
 EOF
